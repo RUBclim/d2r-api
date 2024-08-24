2638e86484dafd22705ce0faee18a6e6875b89c4
Jonas Kittner <jonas.kittner@rub.de>
2024-08-25 01:57:48 +0200

    remove unused schema

--- app/schemas.py
+++ app/schemas.py
@@ -6,12 +6,6 @@ from pydantic import BaseModel
 from app.models import StationType
 
 
-class TempRHMeasurement(BaseModel):
-    measured_on: datetime
-    relative_humidity: float | None
-    air_temperature: float | None
-
-
 class Station(BaseModel):
     name: str
     latitude: float
