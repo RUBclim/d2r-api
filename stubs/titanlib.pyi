898ffb7b6b0b71390a5f5c1e982c7ea4c94b9017
Jonas Kittner <jonas.kittner@rub.de>
2025-06-05 19:16:22 +0200

    add more stubs for titanlib

--- stubs/titanlib.pyi
+++ stubs/titanlib.pyi
@@ -111,3 +111,98 @@ def isolation_check(
         vertical_radius: npt.NDArray[T] | float = float('nan'),
         /,
 ) -> npt.NDArray[np.integer]: ...
+
+
+def range_check_climatology(
+        points: Points,
+        values: npt.NDArray[T],
+        unixtime: int,
+        pos: npt.NDArray[T],
+        neg: npt.NDArray[T],
+        /,
+) -> npt.NDArray[np.integer]: ...
+
+
+def metadata_check(
+        points: Points,
+        check_lat: bool = True,
+        check_lon: bool = True,
+        check_elev: bool = True,
+        check_laf: bool = True,
+) -> npt.NDArray[np.integer]: ...
+
+def range_check(
+        values: npt.NDArray[T],
+        min: npt.NDArray[T],
+        max: npt.NDArray[T],
+) -> npt.NDArray[np.integer]: ...
+
+def sct(
+        points: Points,
+        values: npt.NDArray[T],
+        num_min: int,
+        num_max: int,
+        inner_radius: float,
+        outer_radius: float,
+        num_iterations: int,
+        num_min_prof: int,
+        min_elev_diff: float,
+        min_horizontal_scale: float,
+        vertical_scale: float,
+        pos: npt.NDArray[T],
+        neg: npt.NDArray[T],
+        eps2: npt.NDArray[T],
+        prob_gross_error: npt.NDArray[T],
+        rep: npt.NDArray[T],
+        obs_to_check: Sequence[int] = [],
+) -> npt.NDArray[np.integer]: ...
+
+
+
+def sct_resistant(
+        points: Points,
+        values: npt.NDArray[T],
+        obs_to_check: Sequence[int],
+        background_values: npt.NDArray[T],
+        background_elab_type: str,
+        num_min_outer: int,
+        num_max_outer: int,
+        inner_radius: float,
+        outer_radius: float,
+        num_iterations: int,
+        num_min_prof: int,
+        min_elev_diff: float,
+        min_horizontal_scale: float,
+        max_horizontal_scale: float,
+        kth_closest_obs_horizontal_scale: int,
+        vertical_scale: float,
+        values_mina: npt.NDArray[T],
+        values_maxa: npt.NDArray[T],
+        values_minv: npt.NDArray[T],
+        values_maxv: npt.NDArray[T],
+        eps2: npt.NDArray[T],
+        tpos: npt.NDArray[T],
+        tneg: npt.NDArray[T],
+        debug: bool,
+        basic: bool,
+) -> npt.NDArray[np.integer]: ...
+
+
+def sct_dual(
+        points: Points,
+        values: npt.NDArray[T],
+        obs_to_check: Sequence[int],
+        event_thresholds: npt.NDArray[T],
+        condition: str,
+        num_min_outer: int,
+        num_max_outer: int,
+        inner_radius: float,
+        outer_radius: float,
+        num_iterations: int,
+        min_horizontal_scale: float,
+        max_horizontal_scale: float,
+        kth_closest_obs_horizontal_scale: int,
+        vertical_scale: float,
+        test_thresholds: npt.NDArray[T],
+        debug: bool,
+) -> npt.NDArray[np.integer]: ...
