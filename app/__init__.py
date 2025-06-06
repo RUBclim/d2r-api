87a8160a67d8d0b9c8c81f93c39feb987a3b78a0
Jonas Kittner <jonas.kittner@rub.de>
2025-06-06 12:08:33 +0200

    add aliases for the dashbaord and allow them in CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,3 @@
 # TODO: this is not the best place for this, but we don't want any import side-effects
 # when importing this to terracotta.
-ALLOW_ORIGIN_REGEX = r'https?://([\w\-_]+\.)?(127\.0\.0\.1|localhost|data2resilience|data\-2\-resilience(\-[\w\-_]+\-vogelinos\-projects)?\.vercel)(\.(de|app))?(:\d{2,4})?'  # noqa: E501
+ALLOW_ORIGIN_REGEX = r'https?://([\w\-_]+\.)?(127\.0\.0\.1|localhost|geographie|data2resilience|data\-2\-resilience(\-[\w\-_]+\-vogelinos\-projects)?\.vercel)((\.rub)|(\.ruhr\-uni\-bochum))?(\.(de|app))?(:\d{2,4})?'  # noqa: E501
